b66ca8055dc8f61a055523c9b70e362166a0f5a0
Lautaro <lautigomez177@gmail.com>
2024-11-25 00:00:46 -0300

    Update puntoc.sh

---
+++ puntoc.sh
@@ -0,0 +1,72 @@
+  sudo apt update
+    2  sudo apt upgrade -y
+    3  cd ..
+    4  sudo mkdir docker2Parcial
+    5  cd docker2Parcial/
+    6  sudo mkdir appHomeBanking
+    7  cd appHomeBanking/
+    8  sudo touch index.html
+    9  sudo touch contacto.html
+   10  sudo touch Dockerfile
+   11  nano Dockerfile
+   12  n
+   13  sudo nano Dockerfile
+   14  sudo nano DockerFile
+   15  cd ..
+   16  cd docker2Parcial/
+   17  cd appHomeBanking/
+   18  sudo DockerFile
+   19  sudo nano Dockerfile
+   20  docker login -u lautigomez177
+   21  docker -v
+   22  vagrant haltvagranvagranthalt
+   23  rnntnnraaaaaaaaaaaaaaaaalaaalt
+   24  t halt
+   25  cd ..
+   26  cd docker2Parcial/
+   27  cd appHomeBanking/
+   28  docker login -u lautigomez177
+   29  snap install docker
+   30  sudo snap instal docker
+   31  sudo apt install docker.io
+   32  docker login -u
+   33  docker login -u lautigomez177
+   34  sudo docker login -u lautigomez177
+   35  docker build -t lautigomez177 /2parcial-ayso:v1.0
+   36  docker build -t lautigomez177/2parcial-ayso:v1.0 .
+   37  sudo docker build -t lautigomez177/2parcial-ayso:v1.0 .
+   38  cd ..
+   39  sudo docker build -t lautigomez177/2parcial-ayso:v1.0 .
+   40  sudo touch Dockerfile
+   41  sudo nano Dockerfile
+   42  sudo docker login -u lautigomez177
+   43  sudo docker build -t lautigomez177/2parc
+   44  ls -l Dockerfile
+   45  sudo docker build -t lautigomez177/2parcial-ayso:v1.0 .
+   46  sudo docker build -f /home/docker2Parcial/appHomeBanking/Dockerfile -t la
+utigomez177/2parcial-ayso:v1.0 /home/docker2Parcial
+   47  ls -l Dockerfile
+   48  sudo docker build -t lautigomez177/2parcial-ayso:v1.0 .
+   49  docker image list
+   50  sudo docker image list
+   51  sudo docker push lautigomez177/2parcial-ayso:v1.0
+   52  sudo docker run -d -p 8080:80 lautigomez177/2parcial-ayso:v1.0
+   53  sudo docker containter ls
+   54  sudo docker container ls
+   55  ip a
+   56  sudo docker ps
+   57  sudo docker exec -it 0607fbaa5942 /bin/bash
+   58  sudo docker build -t lautigomez177/2parcial-ayso:v1.0 .
+   59  cd ..
+   60  cd docker2Parcial/
+   61  ls -l
+   62  sudo docker rmi lautigomez177/2parcial-ayso:v1.0
+   63  sudo sudo docker rmi lautigomez177/2parcial-ayso:v1.0
+   64  sudo docker stop 0607fbaa5942
+   65  sudo docker rm 0607fbaa5942
+   66  sudo docker rmi lautigomez177/2parcial-ayso:v1.0
+   67  sudo docker build -t lautigomez177/2parcial-ayso:v1.0 .
+   68  sudo docker run -d -p 8080:80 lautigomez177/2parcial-ayso:v1.0
+   69  docker container ls
+   70  sudo docker container ls
+   71  history
